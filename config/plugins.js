789d6acaac738d3e1d77ec43e465ffc698c4600d
avilla2 <a.villa12@outlook.com>
2025-10-19 12:55:58 -0700

    chore: migrate

--- config/plugins.js
+++ config/plugins.js
@@ -2,13 +2,19 @@ module.exports = ({ env }) => ({
   // ...
   email: {
     config: {
-       provider: 'sendgrid',
-       providerOptions: {
-         apiKey: process.env.SENDGRID_API_KEY
-       },
+      //  provider: 'sendgrid',
+      //  providerOptions: {
+      //    apiKey: process.env.SENDGRID_API_KEY
+      //  },
+      provider: 'amazon-ses',
+      providerOptions: {
+        key: process.env.AWS_SES_KEY,
+        secret: process.env.AWS_SES_SECRET,
+        amazon: `https://email.${process.env.AWS_SES_REGION, 'us-west-2'}.amazonaws.com`, // https://docs.aws.amazon.com/general/latest/gr/ses.html
+      },
        settings: {
          defaultFrom: 'notifications@villawebsolutions.com',
-         defaultReplyTo: 'villalexandro@gmail.com',
+         defaultReplyTo: 'alex@villawebsolutions.com',
        }
      }
    }
