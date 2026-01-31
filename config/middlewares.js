676e0414a0f6dadc7c12d4f5970820bcb5287a84
avilla2 <a.villa12@outlook.com>
2026-01-30 22:42:45 -0800

    chore: add configs

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,32 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            process.env.CDN_URI,
+            `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            process.env.CDN_URI,
+            `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`
+          ],
+          "frame-src": ["'self'"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
